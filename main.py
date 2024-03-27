efda88145833f59e8434761b6abc6f24a88d524a
Marlon-Dantas <92075470+Marlon-Dantas@users.noreply.github.com>
2024-03-27 19:51:46 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import time
 candidatos = [
-    ("aluno1", "e5_t10_p8_s8"),
-    ("aluno2", "e10_t7_p7_s8"),
-    ("aluno3", "e8_t5_p4_s9"), 
-    ("aluno4", "e2_t2_p2_s1"), 
-    ("aluno5", "e10_t10_p8_s9")
+    ("Bruno", "e5_t10_p8_s8"),
+    ("Marlon", "e10_t7_p7_s8"),
+    ("Ana", "e8_t5_p4_s9"), 
+    ("Beatriz", "e2_t2_p2_s1"), 
+    ("Lucas", "e10_t10_p8_s9")
 ]
 
 nota_entrevista_1 = int(input("Coloque a nota da entrevista: "))
@@ -16,12 +16,12 @@ total = []
 
 for aluno in candidatos: 
     nome, notas = aluno
-    e, t, p, s = [int(nota[1:]) for nota in notas.split('_')]
-    if e >= nota_entrevista_1 and t >= nota_teste_teorico_2 and p >= nota_teste_pratico_3 and s >= nota_softskills_4:
-        print(f"O candidato {nome} passou com as notas {e}, {t}, {p}, {s}")
+    entrevista_nota, teorico_nota, pratico_nota, softskills_nota = [int(nota[1:]) for nota in notas.split('_')]
+    if entrevista_nota >= nota_entrevista_1 and teorico_nota >= nota_teste_teorico_2 and pratico_nota >= nota_teste_pratico_3 and softskills_nota >= nota_softskills_4:
+        print(f"O candidato {nome} passou com as notas: {entrevista_nota} na entrevista, {teorico_nota} no teste teorico, {pratico_nota} no teste prático e {softskills_nota} no teste de Softskills.")
         total.append(aluno)
 
 if len(total) == 0:
     print("Nenhum canditado passou")
 
-time.sleep(5)
+input("Pressione a tecla Enter para fechar o programa....")
